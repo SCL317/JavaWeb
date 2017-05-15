6279eddb38a42041b3b2da25a6cf2608897dbff3
SCL <294748882@qq.com>
2017-05-15 19:27:18 +0800

    5.15

---
+++ JavaWebSCL/src/useBeanDemo/HelloServlet.java
@@ -0,0 +1,23 @@
+package useBeanDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+	throws ServletException,IOException
+	{
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>演示</title></head>");
+		out.println("<body>");
+		out.println("<h1>Hello World</h1>");
+		out.println("</body>");
+		out.println("</html>");
+        out.close();
+	}
+}
